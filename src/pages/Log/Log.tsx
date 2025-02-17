7801380e28430fc682c934f7670669865de61c6c
Dilan Fernando <dilanmadushanka6013@gmail.com>
2025-02-17 16:18:18 +0530

    update log is done

--- src/pages/Log/Log.tsx
+++ src/pages/Log/Log.tsx
@@ -33,6 +33,26 @@ const Log = () => {
             }
           });
     }
+
+    const editLog = (log:LogModel)=>{
+      setLogCode(log.logCode);
+      setImage(log.image);
+      setDate(log.date);
+      setField(log.field);
+      setCrop(log.crop);
+      setStaff(log.staff);
+    }
+
+    const updateLog = ()=>{
+      const updatedLogs = logs.map((log)=>{
+        if(log.logCode === logCode){
+          return new LogModel(logCode,image,date,field,crop,staff);
+        }else{
+          return log;
+        }
+      })
+      setLogs(updatedLogs)
+    }
   
   return (
 
@@ -158,8 +178,7 @@ const Log = () => {
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={(e) => {
                 e.preventDefault();
-               
-      
+                updateLog();
               }}
             >
               Update
@@ -214,7 +233,7 @@ const Log = () => {
                     <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer"
                     onClick={(e)=>{
                       e.preventDefault();
-        
+                      editLog(log);
                     }}
                     >
                       Edit
