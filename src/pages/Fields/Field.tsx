d7a7f522a86fa1f818d236cec51419fbf5d1d5e0
Dilan Fernando <dilanmadushanka6013@gmail.com>
2025-02-17 12:11:34 +0530

    update field is done

--- src/pages/Fields/Field.tsx
+++ src/pages/Fields/Field.tsx
@@ -33,7 +33,24 @@ const Field = () => {
 
           }
         });
-    
+  }
+
+  const editField = (field:FieldModel) => {
+    setFieldCode(field.fieldCode)
+    setName(field.name)
+    setLocation(field.location)
+    setSize(field.size)
+  }
+
+  const updateField = () => {
+    const updatedFields = fields.map((field) => {
+      if (field.fieldCode === fieldCode) {
+        return new FieldModel(fieldCode, name, location, size);
+      } else {
+        return field;
+      }
+    });
+    setFields(updatedFields);
   }
 
   return (
@@ -131,6 +148,7 @@ const Field = () => {
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={(e) => {
                 e.preventDefault();
+                updateField();
               }}
             >
               Update
@@ -176,6 +194,10 @@ const Field = () => {
                     </th>
                     <td className="px-6 py-4 flex gap-4">
                     <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer"
+                    onClick={(e)=>{
+                      e.preventDefault();
+                      editField(field)
+                    }}
                     >
                       Edit
                     </span>
