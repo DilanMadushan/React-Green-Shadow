262351a1b8bd4a9495c891e8c9f81c77dfee08f1
Dilan Fernando <dilanmadushanka6013@gmail.com>
2025-02-17 20:45:55 +0530

    upate staff is done

--- src/pages/Staff/Staff.tsx
+++ src/pages/Staff/Staff.tsx
@@ -35,6 +35,30 @@ const Staff = () => {
       });
     };
 
+    const editStaff = (staff: StaffModel) => {
+    setStaffId(staff.staffId);
+    setFirstName(staff.firstName);
+    setLastName(staff.lastName);
+    setDob(staff.dob);
+    setGender(staff.gender);
+    setJoinDate(staff.joinDate);
+    setAddress(staff.address);
+    setMobile(staff.mobile);
+    setEmail(staff.email);
+    }
+
+    const updateStaff = () => {
+    const updatedStaffs = staffs.map((staff)=>{
+      if(staff.staffId === staffId){
+        return new StaffModel(staffId,firstName,lastName,dob,gender,joinDate,address,mobile,email);
+      }else{
+        return staff;
+      }
+    })
+
+    setStaffs(updatedStaffs);
+      }
+
   return (
     <>
       <div className="w-screen h-screen p-10">
@@ -214,6 +238,7 @@ const Staff = () => {
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={(e) => {
                 e.preventDefault();
+                updateStaff();
               }}
             >
               Update
@@ -274,7 +299,7 @@ const Staff = () => {
                     <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer"
                     onClick={(e)=>{
                       e.preventDefault();
-               
+                      editStaff(staff)
                     }}
                     >
                       Edit
