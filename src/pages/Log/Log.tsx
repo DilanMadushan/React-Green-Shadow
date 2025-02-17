535cb2580647d7481cca3ca3c798337f16ff6b91
Dilan Fernando <dilanmadushanka6013@gmail.com>
2025-02-17 16:13:42 +0530

    delete log is done

--- src/pages/Log/Log.tsx
+++ src/pages/Log/Log.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import ImagePicker from "../../Components/ImagePicker";
 import LogModel from '../../Models/LogModel';
+import Swal from "sweetalert2";
 
 const Log = () => {
 
@@ -15,6 +16,22 @@ const Log = () => {
 
     const saveLog = () =>{
       setLogs([...logs,new LogModel(logCode,image,date,field,crop,staff)])
+    }
+
+    const deleteLog = (code:string) =>{
+      Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              setLogs(logs.filter((log)=>(log.logCode != code)))
+            }
+          });
     }
   
   return (
@@ -141,6 +158,7 @@ const Log = () => {
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={(e) => {
                 e.preventDefault();
+               
       
               }}
             >
@@ -204,7 +222,7 @@ const Log = () => {
                     <span className="font-medium text-red-600 dark:text-red-500 hover:underline cursor-pointer"
                     onClick={(e)=>{
                       e.preventDefault();
-                
+                      deleteLog(log.logCode)
                     }}
                     >
                       Delete
