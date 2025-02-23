64ce83c11250ed40f312a06b37276ff9cbe02f59
Dilan Fernando <dilanmadushanka6013@gmail.com>
2025-02-23 15:54:53 +0530

    Vehicle save slice is done is done

---
+++ src/Slice/VehicleSlice.ts
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import VehicleModel from "../Models/VehicleModel";
+import axios from "axios";
+
+const initialState : VehicleModel[] =[];
+
+const api = axios.create({
+    baseURL : "http://localhost:3000/"
+})
+
+
+ export const saveVehicleState = createAsyncThunk(
+    'vehicle/saveVehicleState',
+    async(vehicle:VehicleModel)=>{
+        try{
+            const response = await api.post('vehicle/save',vehicle);
+            return response.data;
+        }catch(e){
+            console.log(e);
+        }
+    }
+)
+
+
+const vehicleSlice = createSlice({
+    name:"vehicle",
+    initialState,
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder.addCase(saveVehicleState.fulfilled,(state,action)=>{
+            state.push(action.payload);
+        })
+        .addCase(saveVehicleState.rejected,(state,action)=>{
+            console.log(action.payload);
+        })
+        .addCase(saveVehicleState.pending,(state,action)=>{
+            console.log("pending");
+        })
+    }
+})
